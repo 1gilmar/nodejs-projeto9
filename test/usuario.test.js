b32a8d1ae87e03022b90b422929162caf461ff0f
1gilmar <1gilmar@gmail.com>
2021-03-16 17:56:24 -0300

    Test cadastro de usuario

---
+++ test/usuario.test.js
@@ -0,0 +1,23 @@
+const app = require('../src/app');
+const superteste = require('supertest');
+let request = superteste(app);
+
+describe("Cadastro de usuario", ()=>{
+
+    test('deve cadastrar um usuario com sucesso', ()=>{
+        
+        let time = Date.now();
+        let email = `${time}@gmail.com`;
+
+        let user = {nome: "gilmar", email, password: "123456"};
+
+        return request.post('/api/user').send(user).then(res =>{
+            expect(res.statusCode).toEqual(200)
+            expect(res.body.email).toEqual(email)
+        }).catch(erro =>{
+            fail(erro);
+        });
+
+    })
+
+})
